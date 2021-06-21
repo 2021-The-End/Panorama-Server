409777aaf99bb6675054ded80d5f0db1119ab800
vinylrich <whktjd0109@gmail.com>
2021-06-21 22:47:05 +0900

    [CHORE] modify database,session model

--- model/model.go
+++ model/model.go
@@ -1,14 +1,36 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	Name      string    `json:"name"`
-	Passwd    string    `json:"password"`
-	CreatedAt time.Time `json:"created_at"`
+	gorm.Model `json:"model,omitempty"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+}
+
+/*
+signup 할 때 User에 들어가야 할 것:
+Accoutusername,Password
+
+Usersession에 expired까지 들어가야 하는 것
+db에 넣을 것인지
+in-memory에 넣을 것인지
+*/
+
+type UserSession struct {
+	SessionKey string
+	User       User
+	Expired    time.Time
 }
 type Post struct {
-	Title     string    `json:"title"`
+	gorm.Model
+	Title  string `json:"title"`
+	Author string `json:"author"`
+
 	Contents  string    `json:"contents"`
 	Imgpaths  []Image   `json:"imgpaths"` //[{imgpaths:"Asd"},{imgpaths:"asdsa"}]
 	CreatedAt time.Time `json:"created_at"`
@@ -19,13 +41,16 @@ type Image struct {
 
 type DBHandler interface {
 	GetUser() *User
-	AddUser(name string) *User
+	AddUser(*User) error
 	RemoveUser(id int)
 	GetPostContents() *Post
 	ModifyPost(*Post)
 	Close()
 	DeleteImg()
 	UploadImg()
+
+	SignupIsUser(User) (bool, error)
+	SigninIsUser(User) (bool, error)
 }
 
 func NewDBHandler() DBHandler {
