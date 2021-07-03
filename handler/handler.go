187c1878166c0017ec119dd4f8a4f52b57bd6090
vinylrich <whktjd0109@gmail.com>
2021-07-03 21:18:05 +0900

    [REFACTOR] throwerr->httputil.NewError

--- handler/handler.go
+++ handler/handler.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"panorama/server/httputil"
 	"panorama/server/model"
 	"panorama/server/utils"
 	"strconv"
@@ -13,33 +14,39 @@ import (
 
 var err error
 
-// Summary Signin
-// Router api/v1/signin [post]
+// Signin godoc
+// @Summary Signin
+// @Description user Signin
+// @Tags Users
+// @Accept  json
+// @Produce  json
+// @Router /user/signin [post]
 func (rh *RouterHandler) signinHandler(c *gin.Context) {
+
 	var user model.User
 	if err = c.ShouldBindJSON(&user); err != nil {
 		// If err occurs BINDING(ENCODING) user err, return serverError
-		utils.ThrowErr(c, http.StatusBadRequest, err)
+		httputil.NewError(c, http.StatusBadRequest, err)
 		return
 	}
 
 	if user.Username == "" {
 		// If binded username is empty, return partialcontent
 		err = errors.New("username is should'nt be empty")
-		utils.ThrowErr(c, http.StatusPartialContent, err)
+		httputil.NewError(c, http.StatusPartialContent, err)
 		return
 	}
 
 	isuser, err := rh.db.SigninIsUser(user)
 	if err != nil {
 		// If err occurs in calling SignInIsUser, return ISE
-		utils.ThrowErr(c, http.StatusPartialContent, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 	if !isuser {
-		// If cant find User, return Unauthorized?
+		// If cant find User, return Unauthorized
 		err = errors.New("user not Found")
-		utils.ThrowErr(c, http.StatusUnauthorized, err)
+		httputil.NewError(c, http.StatusUnauthorized, err)
 		return
 	}
 	var httpwriter http.ResponseWriter = c.Writer
@@ -48,35 +55,41 @@ func (rh *RouterHandler) signinHandler(c *gin.Context) {
 
 	if err != nil {
 		//If err occurs in generating sessioncookie, return ISE
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 	//Signin successfully
 	err = errors.New("login successfully")
-	utils.ThrowErr(c, http.StatusOK, err)
+	httputil.NewError(c, http.StatusOK, err)
+
 }
 
-// Summary Sign up
-// Router api/v1/signup [post]
+// Signup godoc
+// @Summary Signup
+// @Description user Signup
+// @Tags Users
+// @Accept  json
+// @Produce  json
+// @Router /user/signup [post]
 func (rh *RouterHandler) signupHandler(c *gin.Context) {
 	log.Print("call signup handler")
 	var user model.User
 
 	if err := c.ShouldBindJSON(&user); err != nil {
 		// If err occurs BINDING(ENCODING) user err, return serverError
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusBadRequest, err)
 		return
 	}
 	if user.Username == "" {
 		// If binded username is empty, return partialcontent
 		err = errors.New("username is empty")
-		utils.ThrowErr(c, http.StatusPartialContent, err)
+		httputil.NewError(c, http.StatusPartialContent, err)
 		return
 	}
 	hashpwd, err := utils.EncrptPasswd(user.Password)
 	if err != nil {
 		//If err occurs in encrpt passwd, return ISE
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 	user.Password = hashpwd
@@ -84,13 +97,13 @@ func (rh *RouterHandler) signupHandler(c *gin.Context) {
 	isuser, err := rh.db.SignupIsUser(user)
 	if err != nil {
 		//If err occurs in calling SignupIsUser, return ISE
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 	if isuser {
 		//If user already exist, return partialcontent
 		err = errors.New("user alreay exist")
-		utils.ThrowErr(c, http.StatusPartialContent, err)
+		httputil.NewError(c, http.StatusPartialContent, err)
 		return
 
 	}
@@ -98,56 +111,129 @@ func (rh *RouterHandler) signupHandler(c *gin.Context) {
 	err = rh.db.AddUser(&user)
 	if err != nil {
 		//If err occurs in Adding User, return partialcontent
-		utils.ThrowErr(c, http.StatusPartialContent, err)
+		httputil.NewError(c, http.StatusPartialContent, err)
 		return
 	}
 	//Signup successfully
 	err = errors.New("signup successfully")
-	utils.ThrowErr(c, http.StatusCreated, err)
+	httputil.NewError(c, http.StatusCreated, err)
 }
 
-// Summary upload img
-// Description Upload img to public folder to use fileserver
-// Router api/v1/post/img [get]
+// uploadImg godoc
+// @Summary UploadImg
+// @Description Upload Img using fileServer
+// @Tags Img
+// @Accept  json
+// @Produce  json
+// @Router /post/img/upload [post]
 func (rh *RouterHandler) upLoadImgHandler(c *gin.Context) {
 
 	response, err := utils.Validation(c.Request, client)
 	if response == "" {
-		utils.ThrowErr(c, http.StatusUnauthorized, err)
+		httputil.NewError(c, http.StatusUnauthorized, err)
 	}
 	if err != nil {
 		if err == http.ErrNoCookie {
-			utils.ThrowErr(c, http.StatusUnauthorized, err)
+			httputil.NewError(c, http.StatusUnauthorized, err)
 			return
 		}
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 
 	err = utils.UploadFile(c, response)
 	if err != nil {
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 	}
 	err = errors.New("successfully Upload")
-	utils.ThrowErr(c, http.StatusOK, err)
+	httputil.NewError(c, http.StatusOK, err)
 
 }
 
-// Summary get post contents
-// Router api/v1/post/content [get]
-func (rh *RouterHandler) getPostHandler(c *gin.Context) {
+// DeleteImg godoc
+// @Summary DeleteImg
+// @Description DeleteImg posted img
+// @Tags Img
+// @Accept  json
+// @Produce  json
+
+// @Router /post/img [delete]
+func (rh *RouterHandler) deleteImgHandler(c *gin.Context) {
+
+}
+
+// uploadPost godoc
+// @Summary upload Project
+// @Description upload Project
+// @Tags Project
+// @Accept  json
+// @Produce  json
+// @Router /post [post]
+func (rh *RouterHandler) upLoadProjectHandler(c *gin.Context) {
+	var post model.Project
 
 	response, err := utils.Validation(c.Request, client)
 	if response == "" {
-		utils.ThrowErr(c, http.StatusUnauthorized, err)
+		httputil.NewError(c, http.StatusUnauthorized, err)
+	}
+	if err != nil {
+		if err == http.ErrNoCookie {
+			httputil.NewError(c, http.StatusUnauthorized, err)
+			return
+		}
+		httputil.NewError(c, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := c.ShouldBindJSON(&post); err != nil {
+		httputil.NewError(c, http.StatusInternalServerError, err)
+		return
+	}
+	if post.ProjectTitle == "" {
+		// If binded username is empty, return partialcontent
+		err = errors.New("post title empty")
+		httputil.NewError(c, http.StatusPartialContent, err)
+		return
+	}
+	if len(post.Contents) < 20 {
+		err = errors.New("post contents len should belong then 20")
+		httputil.NewError(c, http.StatusPartialContent, err)
+		return
+	}
+	rh.db.UploadPost(&post)
+}
+
+// ModifyPost godoc
+// @Summary ModifyProject
+// @Description ModifyProject
+// @Tags Project
+// @Accept  json
+// @Produce  json
+// @Router /post/:id [patch]
+func (rh *RouterHandler) modifyProjectHandler(c *gin.Context) {
+
+}
+
+// GetPost godoc
+// @Summary GetPost
+// @Description Get Post By Id
+// @Tags Project
+// @Accept  json
+// @Produce  json
+// @Router /post/:id [get]
+func (rh *RouterHandler) getProjectByIdHandler(c *gin.Context) {
+
+	response, err := utils.Validation(c.Request, client)
+	if response == "" {
+		httputil.NewError(c, http.StatusUnauthorized, err)
 		return
 	}
 	if err != nil {
 		if err == http.ErrNoCookie {
-			utils.ThrowErr(c, http.StatusUnauthorized, err)
+			httputil.NewError(c, http.StatusUnauthorized, err)
 			return
 		}
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 		return
 	}
 
@@ -156,68 +242,68 @@ func (rh *RouterHandler) getPostHandler(c *gin.Context) {
 
 	post, err := rh.db.GetbyIdPost(postid)
 
+	if err != nil {
+		httputil.NewError(c, http.StatusInternalServerError, err)
+		return
+	}
 	log.Println(post)
+
+	// for idx, value := range post.Imgpaths {
+
+	// }
+	c.JSON(200, post)
 
 }
 
-func (rh *RouterHandler) getEntirePostHandler(c *gin.Context) {
+func (rh *RouterHandler) reviewHandler(c *gin.Context) {
+
+}
+
+//unresolved yet
+// GetEntirePost godoc
+// @Summary GetEntirePost
+// @Description Get Entire Post to expose introduction page
+// @Tags Project
+// @Accept  json
+// @Produce  json
+// @Router /post [get]
+func (rh *RouterHandler) getEntireProjectHandler(c *gin.Context) {
 	posts, err := rh.db.GetPost()
 	if err != nil {
 		if posts != nil {
-			errors.New("empty contents")
-			utils.ThrowErr(c, http.StatusPartialContent, err)
+			err = errors.New("empty contents")
+			httputil.NewError(c, http.StatusPartialContent, err)
 		}
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
+		httputil.NewError(c, http.StatusInternalServerError, err)
 	}
-	posts.
-		c.JSON(200, gin.H{})
-}
-
-// Summary upload post
-// Router api/v1/post [post]
-func (rh *RouterHandler) upLoadPostHandler(c *gin.Context) {
-	var post model.Post
-
-	response, err := utils.Validation(c.Request, client)
-	if response == "" {
-		utils.ThrowErr(c, http.StatusUnauthorized, err)
-	}
-	if err != nil {
-		if err == http.ErrNoCookie {
-			utils.ThrowErr(c, http.StatusUnauthorized, err)
-			return
-		}
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
-		return
-	}
-
-	if err := c.ShouldBindJSON(&post); err != nil {
-		utils.ThrowErr(c, http.StatusInternalServerError, err)
-		return
-	}
-	if post.ProjectTitle == "" {
-		// If binded username is empty, return partialcontent
-		err = errors.New("post title empty")
-		utils.ThrowErr(c, http.StatusPartialContent, err)
-		return
-	}
-	if len(post.Contents) < 20 {
-		err = errors.New("post contents len should belong then 20")
-		utils.ThrowErr(c, http.StatusPartialContent, err)
-		return
-	}
-	rh.db.UploadPost(&post)
-}
-
-// Summary update post contents
-// Router api/v1/post [patch]
-func (rh *RouterHandler) modifyPostHandler(c *gin.Context) {
+	// for i := 0; i < posts[i]/2; i++ {
+	// 	posts[i]
+	// }
 
 }
 
-// Summary delete img temporary
-// Router api/v1/img [delete]
-func (rh *RouterHandler) deleteImgHandler(c *gin.Context) {
+//unresolved yet
+// uploadComment godoc
+// @Summary Comment upload
+// @Description Comment upload
+// @Tags Comment
+// @Accept  json
+// @Produce  json
+// @Router /comment [post]
+func (rh *RouterHandler) uploadCommentHandler(c *gin.Context) {
+
+}
+
+//unresolved yet
+// GetComment godoc
+// @Summary Get Comment By Id
+// @Description Get Comment By postId
+// @Tags Comment
+// @ID get-string-by-int
+// @Accept  json
+// @Produce  json
+// @Router /comment/:postid [get]
+func (rh *RouterHandler) getCommentHandler(c *gin.Context) {
 
 }
 
