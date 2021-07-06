8a3a6a0a946c7a621c434472442d88347bc315a6
vinylrich <whktjd0109@gmail.com>
2021-07-06 22:34:35 +0900

    [FEAT] FEAT GetByIdPost

--- model/sqlHandler.go
+++ model/sqlHandler.go
@@ -1,41 +1,56 @@
 package model
 
 import (
+	"database/sql"
 	"errors"
+	"fmt"
 	"log"
 	"panorama/server/utils"
 	"time"
 
+	"panorama/server/dbinfo"
+
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type postgreHandler struct {
-	db *gorm.DB
+	sqldb *sql.DB
+	ormdb *gorm.DB
 }
 
+var dsn = fmt.Sprintf("host=%s port=%d user=%s "+
+	"password=%s dbname=%s sslmode=disable",
+	dbinfo.Host, dbinfo.Port, dbinfo.User, dbinfo.Password, dbinfo.Dbname)
+
 func NewPostgreHandler() DBHandler {
-	dsn := "host=localhost user=postgres password=rlawnsdn6! dbname=panorama port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	sqldb, err := sql.Open("postgres", dsn)
+	if err != nil {
+		panic("failed to connect sql database")
+	}
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		dbinfo.Host, dbinfo.Port, dbinfo.User, dbinfo.Password, dbinfo.Dbname)
+	ormdb, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
 	if err != nil {
 
-		panic("failed to connect database")
+		panic("failed to connect orm database")
 
 	}
-	return &postgreHandler{db: db}
+	return &postgreHandler{sqldb: sqldb, ormdb: ormdb}
 }
 
 //if value is not user, return false
 func (p *postgreHandler) SignupIsUser(user User) (bool, error) {
 	log.Println("call model/SignupIsUser")
 	result := user
-
-	result.CreatedAt = time.Now()
-	result.UpdatedAt = time.Now()
 	// Get first matched record
-	if err := p.db.Where("username = ?", result.Username).First(&result).Error; err != nil {
+	if err := p.ormdb.Where("username = ?", result.Username).First(&result).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return false, nil
+			return false, nil //false여야만 signup
 		}
 		return true, err
 	} else {
@@ -50,13 +65,8 @@ func (p *postgreHandler) SigninIsUser(user User) (bool, error) {
 	log.Println("call model/SigninIsUser")
 	result := &User{}
 
-	result.CreatedAt = time.Now()
-	result.UpdatedAt = time.Now()
 	// Get first matched record
-	if err := p.db.Where("username = ?", user.Username).First(&result).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return true, nil //false여야만 singin
-		}
+	if err := p.ormdb.Where("username = ?", user.Username).First(&result).Error; err != nil {
 		return false, err
 	} else {
 		if !utils.CompareHash(result.Password, user.Password) || result.Username != user.Username { //db에 있는 username이 json으로 받은 username과 달라야 false 리턴
@@ -73,17 +83,16 @@ func (p *postgreHandler) GetUser() *User {
 
 func (p *postgreHandler) AddUser(user *User) error { //If json.UserName is not equal with db.username AddUser call
 	log.Print("call model/AddUser")
-	user.CreatedAt = time.Now()
-	user.UpdatedAt = time.Now()
-	if err := p.db.Create(&user).Error; err != nil {
+
+	if err := p.ormdb.Create(&user).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (p *postgreHandler) GetPost() (*[]Post, error) {
-	post := &[]Post{}
-	if err := p.db.Find(&post).Error; err != nil {
+func (p *postgreHandler) GetPost() (*[]ProjectSum, error) {
+	post := &[]ProjectSum{}
+	if err := p.ormdb.Find(&post).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, nil
 		}
@@ -93,7 +102,7 @@ func (p *postgreHandler) GetPost() (*[]Post, error) {
 }
 
 func (p *postgreHandler) RemoveUser(username string) error {
-	if err := p.db.Delete(&User{}, username).Error; err != nil {
+	if err := p.ormdb.Delete(&User{}, username).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil
 		}
@@ -102,28 +111,50 @@ func (p *postgreHandler) RemoveUser(username string) error {
 	return nil
 }
 
-func (p *postgreHandler) GetbyIdPost(postid int) (*Post, error) {
-	result := &Post{}
-	if err := p.db.Where("post_id = ?", postid).First(&result).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, err
-		}
+func (p *postgreHandler) GetbyIdPost(projectid int) (*Projectcon, error) {
+	Projectcon := &Projectcon{}
+	var img []string
+	var cre []string
+	selImgQuery := dbinfo.SelImgpathQuery
+	selCreQuery := dbinfo.SelCreaterQuery
+
+	err := p.sqldb.QueryRow(selImgQuery, projectid).Scan(pq.Array(&img))
+	if err != nil {
 		return nil, err
 	}
-	return result, nil
+
+	Projectcon.Imgpaths = img
+	log.Println(img)
+
+	err = p.sqldb.QueryRow(selCreQuery, projectid).Scan(pq.Array(&cre))
+	if err != nil {
+		return nil, err
+	}
+	Projectcon.Creaters = cre
+	log.Println(cre)
+
+	if err := p.ormdb.Omit("Imgpaths", "Creaters").Table("projectcon").First(&Projectcon, "id = ?", projectid).Error; err != nil {
+		return nil, err
+	}
+	return Projectcon, nil
 }
 
-func (p *postgreHandler) UploadPost(post *Post) error {
+func (p *postgreHandler) UploadPost(Projectcon *Projectcon) error {
 	log.Print("call model/UploadPost")
-	post.CreatedAt = time.Now()
-	post.UpdatedAt = time.Now()
-	if err := p.db.Create(&post).Error; err != nil {
+	Projectcon.CreatedAt = time.Now()
+	Projectcon.UpdatedAt = time.Now()
+
+	insQuery := dbinfo.InsQuery
+	_, err := p.sqldb.Exec(insQuery, Projectcon.Title, Projectcon.Contents,
+		pq.Array(Projectcon.Creaters), pq.Array(Projectcon.Imgpaths), Projectcon.Summary, Projectcon.Grade, Projectcon.CreatedAt, Projectcon.UpdatedAt)
+
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (p *postgreHandler) ModifyPost(*Post) {
+func (p *postgreHandler) ModifyPost(*Projectcon) {
 
 }
 
@@ -131,4 +162,5 @@ func (p *postgreHandler) DeleteImg() {
 
 }
 func (p *postgreHandler) Close() {
+	p.sqldb.Close()
 }
